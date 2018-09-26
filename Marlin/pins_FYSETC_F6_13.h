d237f0ee89951a35e723f883b80f4a9440dd4885
Mitch Roote <mitch@r00t.ca>
2018-09-26 11:31:54 -0400

    Fix some pin assignments, cleanup formatting

--- Marlin/pins_FYSETC_F6_13.h
+++ Marlin/pins_FYSETC_F6_13.h
@@ -20,10 +20,9 @@
  *
  */
 
-/**
- * Fysetc F6 pin assignments
- */
-
+//
+// Fysetc F6 pin assignments
+//
 #ifndef __AVR_ATmega2560__
   #error "Oops!  Make sure you have 'FYSETC_F6' selected from the 'Tools -> Boards' menu."
 #endif
@@ -40,7 +39,7 @@
 #define Y_MIN_PIN          14
 #define Y_MAX_PIN          15
 #define Z_MIN_PIN          12
-#define Z_MAX_PIN          9
+#define Z_MAX_PIN          2
 
 //
 // Z Probe (when not Z_MIN_PIN)
@@ -76,11 +75,10 @@
 #define E2_DIR_PIN         48
 #define E2_ENABLE_PIN      58
 
-
-/* DIAG
- * DIAG is not directly connected to the MCU. Please close
- * the jumper next to the limit switch socket when using it.
- */
+//
+// DIAG is not directly connected to the MCU. Please close
+// the jumper next to the limit switch socket when using it.
+//
 #define X_CS_PIN        32
 #define Y_CS_PIN        39
 #define Z_CS_PIN        42
@@ -98,10 +96,7 @@
 
 
 #if HAS_DRIVER(TMC2208)
-/**
- * Software serial
- */
-
+ // Software serial
  #define X_SERIAL_RX_PIN       71
  #define X_SERIAL_TX_PIN       72
 
@@ -148,9 +143,9 @@
 #define LED_PIN            13
 
 #if ENABLED(RGB_LED) || ENABLED(RGBW_LED)
-  #define RGB_LED_R_PIN    44
-  #define RGB_LED_G_PIN    45
-  #define RGB_LED_B_PIN    46
+  #define RGB_LED_R_PIN    3
+  #define RGB_LED_G_PIN    4
+  #define RGB_LED_B_PIN    9
   #define RGB_LED_W_PIN    -1
 #endif
 
@@ -166,34 +161,9 @@
   #define PS_ON_PIN         11
 #endif
 
-//#if ENABLED(CASE_LIGHT_ENABLE) && !PIN_EXISTS(CASE_LIGHT) && !defined(SPINDLE_LASER_ENABLE_PIN)
-//  #if !defined(NUM_SERVOS) || NUM_SERVOS == 0 // try to use servo connector first
-//    #define CASE_LIGHT_PIN   6      // MUST BE HARDWARE PWM
-//  #elif !(ENABLED(ULTRA_LCD) && ENABLED(NEWPANEL) \
-//      && (ENABLED(PANEL_ONE) || ENABLED(VIKI2) || ENABLED(miniVIKI) || ENABLED(MINIPANEL) || ENABLED(REPRAPWORLD_KEYPAD)))  // try to use AUX 2
-//    #define CASE_LIGHT_PIN   44     // MUST BE HARDWARE PWM
-//  #endif
-//#endif
-
 //
-// M3/M4/M5 - Spindle/Laser Control
-//
-//#if ENABLED(SPINDLE_LASER_ENABLE) && !PIN_EXISTS(SPINDLE_LASER_ENABLE)
-//  #if !defined(NUM_SERVOS) || NUM_SERVOS == 0 // try to use servo connector first
-//    #define SPINDLE_LASER_ENABLE_PIN  4  // Pin should have a pullup/pulldown!
-//    #define SPINDLE_LASER_PWM_PIN     6  // MUST BE HARDWARE PWM
-//    #define SPINDLE_DIR_PIN           5
-//  #elif !(ENABLED(ULTRA_LCD) && ENABLED(NEWPANEL) \
-//      && (ENABLED(PANEL_ONE) || ENABLED(VIKI2) || ENABLED(miniVIKI) || ENABLED(MINIPANEL) || ENABLED(REPRAPWORLD_KEYPAD)))  // try to use AUX 2
-//    #define SPINDLE_LASER_ENABLE_PIN 40  // Pin should have a pullup/pulldown!
-//    #define SPINDLE_LASER_PWM_PIN    44  // MUST BE HARDWARE PWM
-//    #define SPINDLE_DIR_PIN          65
-//  #endif
-//#endif
-
-//////////////////////////
 // LCDs and Controllers //
-//////////////////////////
+//
 #define LCD_PINS_RS         16
 #define LCD_PINS_ENABLE     17
 #define LCD_PINS_D4         23
